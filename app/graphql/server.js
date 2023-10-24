f3001de2302a9cf912cdd32e36d74d537cb9ac89
hadidefra <147404223+hadidefra@users.noreply.github.com>
2023-10-24 15:24:30 +0100

    Sekelton Fix for Node Module (#8)

---
+++ app/graphql/server.js
@@ -0,0 +1,28 @@
+import { dirname, join } from 'path'
+import { fileURLToPath } from 'url'
+
+import { loadFiles } from '@graphql-tools/load-files'
+import { makeExecutableSchema } from '@graphql-tools/schema'
+import { ApolloServer } from '@apollo/server'
+import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
+import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
+
+const graphqlPath = dirname(fileURLToPath(import.meta.url))
+
+export const schema = makeExecutableSchema({
+  typeDefs: await loadFiles(join(graphqlPath, 'types')),
+  resolvers: await loadFiles(join(graphqlPath, 'resolvers'))
+})
+
+export const apolloServer = new ApolloServer({
+  schema,
+  plugins: [
+    (() => {
+      if (process.env?.NODE_ENV === 'production') {
+        return ApolloServerPluginLandingPageDisabled()
+      }
+
+      return ApolloServerPluginLandingPageLocalDefault()
+    })()
+  ]
+})
--- app/server.js
+++ app/server.js
@@ -1,9 +1,3 @@
-import { ApolloServer } from '@apollo/server'
-import { ApolloServerPluginLandingPageDisabled } from '@apollo/server/plugin/disabled'
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default'
-import { dirname, join } from 'path'
-import { loadFilesSync } from '@graphql-tools/load-files'
-import { pathToFileURL, fileURLToPath } from 'url'
 import hapi from '@hapi/hapi'
 
 import { healthyRoute } from './routes/healthy.js'
@@ -11,31 +5,6 @@ import { healthzRoute } from './routes/healthz.js'
 import { setupAppInsights } from './insights.js'
 
 setupAppInsights()
-
-const __dirname = dirname(fileURLToPath(import.meta.url))
-
-const typeDefs = loadFilesSync(join(__dirname, 'graphql', 'types'), {
-  recursive: true
-})
-
-const resolvers = loadFilesSync(join(__dirname, 'graphql', 'resolvers'), {
-  recursive: true,
-  requireMethod: async path => import(pathToFileURL(path))
-})
-
-export const apolloServer = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [
-    (() => {
-      if (process.env?.NODE_ENV === 'production') {
-        return ApolloServerPluginLandingPageDisabled()
-      }
-
-      return ApolloServerPluginLandingPageLocalDefault()
-    })()
-  ]
-})
 
 export const server = hapi.server({
   port: process.env.PORT
