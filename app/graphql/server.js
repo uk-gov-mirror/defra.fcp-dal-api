90d85b1649525213335a4f3494d029742f52f207
Hadi <hadi.tajallaei@defra.gov.uk>
2023-10-25 14:01:46 +0100

    added error retorpting for restriction

--- app/graphql/server.js
+++ app/graphql/server.js
@@ -12,31 +12,12 @@ const graphqlPath = dirname(fileURLToPath(import.meta.url))
 export const schema = makeExecutableSchema({
   typeDefs: await loadFiles(join(graphqlPath, 'types')),
   resolvers: await loadFiles(join(graphqlPath, 'resolvers')),
-  resolverValidationOptions: (function () {
-    return {
-      requireResolversForArgs: 'ignore',
-      /**
-       * Enable to require a resolver to be defined for any field which has
-       * a return type that isn't a scalar. Defaults to `ignore`.
-       */
-      requireResolversForNonScalar: 'ignore',
-      /**
-       * Enable to require a resolver for be defined for all fields defined
-       * in the schema. Defaults to `ignore`.
-       */
-      requireResolversForAllField: 'ignore',
-      /**
-       * Enable to require a `resolveType()` for Interface and Union types.
-       * Defaults to `ignore`.
-       */
-      requireResolversForResolveType: 'ignore',
-      /**
-       * Enable to require all defined resolvers to match fields that
-       * actually exist in the schema. Defaults to `error` to catch common errors.
-       */
-      requireResolversToMatchSchema: 'ignore',
-    }
-  })()
+  resolverValidationOptions: {
+    requireResolversForArgs: 'error',
+    requireResolversForAllField: 'error',
+    requireResolversForResolveType: 'error',
+    requireResolversToMatchSchema: 'error',
+  }
 })
 
 export const apolloServer = new ApolloServer({
