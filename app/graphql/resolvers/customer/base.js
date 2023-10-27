adc287e0069ffb75d8d9e14e11640aef9daeeff0
jack-mccabe-defra <147545717+jack-mccabe-defra@users.noreply.github.com>
2023-10-27 08:56:08 +0100

    Update app/graphql/resolvers/customer/base.js
    Co-authored-by: Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>

--- app/graphql/resolvers/customer/base.js
+++ app/graphql/resolvers/customer/base.js
@@ -2,7 +2,7 @@ import { findCustomerByReferenceHandler } from '../../../core/customer/query.js'
 import { updateCustomerAuthenticateHandler } from '../../../core/customer/mutation.js'
 
 export const Query = {
-  customer: function (_, { referenceNumber }) {
+  customer (_, { referenceNumber }) {
     return findCustomerByReferenceHandler(referenceNumber)
   }
 }
