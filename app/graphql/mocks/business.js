40828e4c0a3ffebd4442ec6b9a6b8fabf5090f59
Hadi <hadi.tajallaei@defra.gov.uk>
2023-11-13 09:35:00 +0000

    created a stand alone mock for land

--- app/graphql/mocks/business.js
+++ app/graphql/mocks/business.js
@@ -1,14 +1,7 @@
 import { faker } from '@faker-js/faker/locale/en_GB'
+import { LandManagement } from './land.js'
 
 export const Business = {
   id: faker.string.alphanumeric({ length: 10, casing: 'upper' }),
-  landManagement: {
-    summary: {
-      date: faker.date.anytime().toUTCString(),
-      numberOfParcels: faker.number.int(),
-      totalArea: faker.number.float(),
-      mapImageData: faker.lorem.text(),
-      permanentGrassland: faker.number.float()
-    }
-  }
+  landManagement: LandManagement
 }
---
+++ app/graphql/mocks/land.js
@@ -0,0 +1,11 @@
+import { faker } from '@faker-js/faker/locale/en_GB'
+
+export const LandManagement = {
+  summary: {
+    date: faker.date.anytime().toUTCString(),
+      numberOfParcels: faker.number.int(),
+      totalArea: faker.number.float(),
+      mapImageData: faker.lorem.text(),
+      permanentGrassland: faker.number.float()
+  }
+}
