173500a1817364ec47954df4f6d4ba3e0cce95dd
Hadi <hadi.tajallaei@defra.gov.uk>
2023-11-13 09:37:29 +0000

    created a stand alone mock for scalars

--- app/graphql/mocks/common.js
+++ app/graphql/mocks/common.js
@@ -1,11 +1,5 @@
 import { faker } from '@faker-js/faker/locale/en_GB'
 
-export const Date = () => (faker.date.anytime().toString())
-
-export const Image = () => (faker.number.hex())
-
-export const Numeric = () => (faker.number.int())
-
 export const Phone = () => ({
   mobile: faker.phone.number(),
   landline: faker.phone.number(),
