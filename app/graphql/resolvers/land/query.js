3c4b1b650adcf7ed61b9647e04baefab41047d72
Hadi <hadi.tajallaei@defra.gov.uk>
2023-11-13 08:53:18 +0000

    removed mocked field in an actual resolver

--- app/graphql/resolvers/land/query.js
+++ app/graphql/resolvers/land/query.js
@@ -1,13 +1,7 @@
 export const Query = {
   landManagement: function () {
     return {
-      summary: {
-        totalArea: 2323.1,
-        date: '2022-08-08',
-        numberOfParcels: 28,
-        mapImageData: 'REVGUkFJbWFnZVNjYWxhckdyYXBoUUxUeXBl',
-        permanentGrassland: 8112.5
-      }
+      summary: {}
     }
   }
 }
