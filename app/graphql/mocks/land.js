6c9e05964383692120d451924def85c3158c2a19
Hadi <hadi.tajallaei@defra.gov.uk>
2023-11-13 09:40:14 +0000

    resolved linting issue

--- app/graphql/mocks/land.js
+++ app/graphql/mocks/land.js
@@ -3,9 +3,9 @@ import { faker } from '@faker-js/faker/locale/en_GB'
 export const LandManagement = {
   summary: {
     date: faker.date.anytime().toUTCString(),
-      numberOfParcels: faker.number.int(),
-      totalArea: faker.number.float(),
-      mapImageData: faker.lorem.text(),
-      permanentGrassland: faker.number.float()
+    numberOfParcels: faker.number.int(),
+    totalArea: faker.number.float(),
+    mapImageData: faker.lorem.text(),
+    permanentGrassland: faker.number.float()
   }
 }
