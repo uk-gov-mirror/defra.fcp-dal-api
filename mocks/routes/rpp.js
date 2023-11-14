0a262dab06793b625b32aeba68d0b5c985176e69
Dominic Taylor <dom.taylor7@gmail.com>
2023-11-14 15:25:05 +0000

    add business mock

--- mocks/routes/rpp.js
+++ mocks/routes/rpp.js
@@ -119,14 +119,82 @@ module.exports = [
   {
     id: "rpp-api-organisation",
     url: "/rpp/api/organisation/:orgId",
-    method: ["POST"],
+    method: ["GET"],
     variants: [
       {
         id: "default",
         type: "json",
         options: {
           status: 200,
-          body: organisationResponse,
+          body: {
+            "_data": {
+              "id": faker.string.numeric(7),
+              "name": faker.company.name(),
+              "sbi": parseInt(faker.string.numeric(9)),
+              // "additionalSbiIds": [],
+              "confirmed": false,
+              // "lastUpdatedOn": 1645694991141,
+              // "landConfirmed": true,
+              "deactivated": false,
+              "locked": false,
+              "address": {
+                  "address1": null,
+                  "address2": null,
+                  "address3": null,
+                  "address4": null,
+                  "address5": null,
+                  "pafOrganisationName": null,
+                  "flatName": null,
+                  "buildingNumberRange": null,
+                  "buildingName": null,
+                  "street": null,
+                  "city": "HEATHFIELD",
+                  "county": null,
+                  "postalCode": "TA2 8LH",
+                  "country": "United Kingdom",
+                  "uprn": "200001642435",
+                  "dependentLocality": null,
+                  "doubleDependentLocality": null,
+                  "addressTypeId": null
+              },
+              // "correspondenceAddress": null,
+              // "isFinancialToBusinessAddr": null,
+              // "isCorrespondenceAsBusinessAddr": null,
+              "email": "lynehouseliveryq@yrevilesuohenyli.com.test",
+              "emailValidated": false,
+              "landline": "01234394643",
+              "mobile": null,
+              "fax": null,
+              // "correspondenceEmail": null,
+              // "correspondenceEmailValidated": false,
+              // "correspondenceLandline": null,
+              // "correspondenceMobile": null,
+              // "correspondenceFax": null,
+              // "taxRegistrationNumber": null,
+              "businessType": {
+                  "id": 101443,
+                  "type": "Not Specified"
+              },
+              "businessReference": "1100974309",
+              "legalStatus": {
+                  "id": 102111,
+                  "type": "Sole Proprietorship"
+              },
+              
+              // "dateStartedFarming": null,
+              "companiesHouseRegistrationNumber": null,
+              "charityCommissionRegistrationNumber": null,
+              // "persons": [],
+              // "hasLandInNorthernIreland": null,
+              // "hasLandInScotland": null,
+              // "hasLandInWales": null,
+              // "hasAdditionalBusinessActivities": null,
+              // "vendorNumber": "329756G",
+              // "traderNumber": null,
+              // "isAccountablePeopleDeclarationCompleted": null,
+              // "additionalBusinessActivities": null
+            }
+          },
         }
       }
     ]
