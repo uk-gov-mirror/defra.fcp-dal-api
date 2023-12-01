e0d093df154c3fb62a85766b2311fb85f621ae09
Dominic Taylor <dom.taylor7@gmail.com>
2023-12-01 14:31:52 +0000

    update helper

--- test/helper.js
+++ test/helper.js
@@ -1,6 +1,6 @@
 import mockServer from '../mocks/server.js'
 
 if (process.env.ENABLE_MOCK_SERVER) {
-  before(mockServer.start)
-  after(mockServer.stop)
+  before(async () => mockServer.start())
+  after(async () => mockServer.stop())
 }
