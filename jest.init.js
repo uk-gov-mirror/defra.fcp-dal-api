abfe2ea203db16ace53230d33169c5e0e9b84aea
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2024-08-07 15:05:50 +0100

    remove faker package

--- jest.init.js
+++ jest.init.js
@@ -1,10 +1,7 @@
-import { faker } from '@faker-js/faker/locale/en_GB'
-
 import mockServer from './mocks/server'
 
 if (process.env.ENABLE_MOCK_SERVER) {
   beforeAll(async () => {
-    faker.seed(5109389384975743)
     await mockServer.start()
   })
   afterAll(mockServer.stop)
