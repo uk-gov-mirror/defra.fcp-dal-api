459c4fd25090d3d5dc1e94f2240e1afff997f4d3
jack-mccabe-defra <66681105+j-mccabe@users.noreply.github.com>
2024-08-23 15:06:05 +0100

    add additional unauthenticated tests

--- test/integration/auth/unauthenticated.test.js
+++ test/integration/auth/unauthenticated.test.js
@@ -4,7 +4,7 @@ import { context } from '../../../app/graphql/context.js'
 import { schema } from '../../../app/graphql/server.js'
 
 describe('Query.customer without authorization header', () => {
-  it('should return customer data', async () => {
+  it('customer query should return an auth error', async () => {
     const unAuthedContext = await context({})
     const result = await graphql({
       source: `#graphql
@@ -27,6 +27,56 @@ describe('Query.customer without authorization header', () => {
       contextValue: unAuthedContext
     })
     expect(result.data.customer).toBeNull()
+    expect(result.errors).toHaveLength(1)
+  })
+
+  it('business query should return an auth error', async () => {
+    const unAuthedContext = await context({})
+    const result = await graphql({
+      source: `#graphql
+        query Business {
+          business(sbi: "107183280") {
+            land {
+              parcels {
+                id
+                sheetId
+                area
+              }
+            }
+          }
+        }
+      `,
+      schema,
+      contextValue: unAuthedContext
+    })
+    expect(result.data.business).toBeNull()
+    expect(result.errors).toHaveLength(1)
+  })
+
+  it('permissions query should return an auth error', async () => {
+    const unAuthedContext = await context({})
+    const result = await graphql({
+      source: `#graphql
+          query PermissionGroups($crn: ID!, $sbi: ID!) {
+            permissionGroups {
+              id
+              name
+              permissions {
+                active(crn: $crn, sbi: $sbi)
+                functions
+                level
+              }
+            }
+          }
+        `,
+      variableValues: {
+        crn: '1102634220',
+        sbi: '107183280'
+      },
+      schema,
+      contextValue: unAuthedContext
+    })
+    expect(result.data.permissionGroups).toBeNull()
     expect(result.errors).toHaveLength(1)
   })
 })
