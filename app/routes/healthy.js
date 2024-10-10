092d0935bef0214b304726052a81f3e7d033a000
Dominic Taylor <dom.taylor@defra.gov.uk>
2024-10-10 14:45:01 +0100

    health check updates
    - add ability to disable
    - if health checks are disabled on prod log error
    - log error if missing env vars for health checks
    - update .env.example with new env vars

--- app/routes/healthy.js
+++ app/routes/healthy.js
@@ -2,6 +2,7 @@ import { StatusCodes } from 'http-status-codes'
 import { AuthenticateDatabase } from '../../app/data-sources/authenticate/AuthenticateDatabase.js'
 import { EntraIdApi } from '../../app/data-sources/entra-id/EntraIdApi.js'
 import { RuralPaymentsBusiness } from '../../app/data-sources/rural-payments/RuralPaymentsBusiness.js'
+import { DAL_HEALTH_CHECK_001 } from '../logger/codes.js'
 import { logger } from '../logger/logger.js'
 
 const minute = 60 * 1000
@@ -42,23 +43,47 @@ const entraHealthCheck = throttle(async () => {
   return entraIdApi.getEmployeeId(process.env.ENTRA_HEALTH_CHECK_USER_OBJECT_ID)
 }, process.env.HEALTH_CHECK_ENTRA_THROTTLE_TIME_MS || fiveMinutes)
 
-// TODO: implement health check for CRM
-const crmHealthCheck = () => true
-
 export const healthyRoute = {
   method: 'GET',
   path: '/healthy',
   handler: async (_request, h) => {
     try {
-      await Promise.all([
-        authenticateDatabaseHealthCheck(),
-        ruralPaymentsAPIMHealthCheck(),
-        entraHealthCheck(),
-        crmHealthCheck()
-      ])
+      if (process.env.HEALTH_CHECK_ENABLED === 'true') {
+        const healthChecks = []
+
+        if (process.env.RP_INTERNAL_HEALTH_CHECK_ORGANISATION_ID?.length) {
+          healthChecks.push(ruralPaymentsAPIMHealthCheck())
+        } else {
+          logger.error('#health check - missing environment variable "RP_INTERNAL_HEALTH_CHECK_ORGANISATION_ID"', { code: DAL_HEALTH_CHECK_001 })
+        }
+
+        if (process.env.ENTRA_HEALTH_CHECK_USER_OBJECT_ID?.length) {
+          healthChecks.push(entraHealthCheck())
+        } else {
+          logger.error('#health check - missing environment variable "ENTRA_HEALTH_CHECK_USER_OBJECT_ID"', { code: DAL_HEALTH_CHECK_001 })
+        }
+
+        if (
+          process.env.AUTHENTICATE_DB_USERNAME?.length,
+          process.env.AUTHENTICATE_DB_PASSWORD?.length,
+          process.env.AUTHENTICATE_DB_USERNAME_AUDIT_WRITE?.length,
+          process.env.AUTHENTICATE_DB_PASSWORD_AUDIT_WRITE?.length
+        ) {
+          healthChecks.push(authenticateDatabaseHealthCheck())
+        } else {
+          logger.error('#health check - missing one or all environment variables "AUTHENTICATE_DB_USERNAME","AUTHENTICATE_DB_PASSWORD","AUTHENTICATE_DB_USERNAME_AUDIT_WRITE","AUTHENTICATE_DB_PASSWORD_AUDIT_WRITE"', { code: DAL_HEALTH_CHECK_001 })
+        }
+
+        await Promise.all(healthChecks)
+      } else if (process.env.ENVIRONMENT === 'prd1') {
+        logger.error('#health check - health check disabled', { code: DAL_HEALTH_CHECK_001 })
+      } else {
+        logger.warn('#health check - health check disabled', { code: DAL_HEALTH_CHECK_001 })
+      }
 
       return h.response('ok').code(StatusCodes.OK)
     } catch (error) {
+      logger.error('#health check - error', { error, code: DAL_HEALTH_CHECK_001 })
       return h.response('error').code(StatusCodes.INTERNAL_SERVER_ERROR)
     }
   }
