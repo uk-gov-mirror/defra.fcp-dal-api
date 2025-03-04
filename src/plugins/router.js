33564f2ed43d548966c41c9f1c41c4505e50875d
Robin Knipe <robin.knipe@defra.gov.uk>
2025-03-04 19:37:56 +0000

    chore: remove unused template contents

--- src/plugins/router.js
+++ src/plugins/router.js
@@ -1,13 +1,10 @@
 import { health } from '../routes/health.js'
-import { example } from '../routes/example.js'
 
 const router = {
   plugin: {
     name: 'router',
     register: (server, _options) => {
-      server.route([health].concat(
-        example
-      ))
+      server.route([health])
     }
   }
 }
