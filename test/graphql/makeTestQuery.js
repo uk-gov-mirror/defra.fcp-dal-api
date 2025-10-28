745bd6e9b863c173599b7d2b55407729b2476eb0
jack-mccabe-defra <jack.mccabe@defra.gov.uk>
2025-10-28 09:33:42 +0000

    fix test query

--- test/graphql/makeTestQuery.js
+++ test/graphql/makeTestQuery.js
@@ -7,14 +7,15 @@ export async function makeTestQuery(
   source,
   isAuthenticated = true,
   variableValues = {},
-  headers = { email: 'test@defra.gov.uk', 'gateway-type': 'internal' }
+  headers = { email: 'test@defra.gov.uk', 'gateway-type': 'internal' },
+  authGroups = []
 ) {
   return graphql({
     source,
     schema: await createSchema(),
     contextValue: {
       ...(await context({ request: { headers } })),
-      auth: isAuthenticated ? { groups: [config.get('auth.groups.ADMIN')] } : {}
+      auth: isAuthenticated ? { groups: [config.get('auth.groups.ADMIN')] } : { groups: authGroups }
     },
     variableValues: variableValues
   })
